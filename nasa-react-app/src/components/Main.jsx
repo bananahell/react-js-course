c68c00c9bb8ca31651640bc2066f9d04eb6a20e5
Pedro Nogueira <cmcg89034@gmail.com>
2024-10-29 10:56:22 -0300

    Completed second project, with weird sidebar...

---
+++ nasa-react-app/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main(props) {
+  const { data } = props;
+
+  return (
+    <div className="imgContainer">
+      <img
+        src={data?.hdurl}
+        alt={data?.title || "bg-img"}
+        className="bg-image"
+      />
+    </div>
+  );
+}
